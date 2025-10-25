caf764298c407eb03eda0196863c08d0d2de701e
Carlos Delgadillo <cardel91@gmail.com>
2025-10-24 22:09:58 -0600

    JWT Login and messages ep292-294

--- src/auth/pages/login/LoginPage.tsx
+++ src/auth/pages/login/LoginPage.tsx
@@ -3,14 +3,46 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { CustomLogo } from "@/components/custom/CustomLogo"
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
+import { useState, type FormEvent } from "react"
+import { loginAction } from "@/auth/actions/login.action"
+import { toast } from "sonner"
 
 export const LoginPage = () => {
+
+    const navigate = useNavigate();
+    const [isPosting, setIsPosting] = useState(false);
+
+    const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setIsPosting(true);
+        const formData = new FormData(event.target as HTMLFormElement);
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+
+        // console.log({ email, password });
+
+
+        try {
+            const data = await loginAction(email, password);
+            console.log({ data });
+            localStorage.setItem('token', data.token);
+            console.log('redirect home');
+            navigate('/');
+
+        } catch {
+            toast.error('Invalid credentials');
+        }
+
+        setIsPosting(false);
+
+    }
+
     return (
         <div className={"flex flex-col gap-6"}>
             <Card className="overflow-hidden p-0" >
                 <CardContent className="grid p-0 md:grid-cols-2">
-                    <form className="p-6 md:p-8">
+                    <form className="p-6 md:p-8" onSubmit={handleLogin}>
                         <div className="flex flex-col gap-6">
                             <div className="flex flex-col items-center text-center">
                                 <CustomLogo />
@@ -18,7 +50,7 @@ export const LoginPage = () => {
                             </div>
                             <div className="grid gap-2">
                                 <Label htmlFor="email">Email</Label>
-                                <Input id="email" type="email" placeholder="user@domain.com" required />
+                                <Input id="email" type="email" name="email" placeholder="user@domain.com" required />
                             </div>
                             <div className="grid gap-2">
                                 <div className="flex items-center">
@@ -27,9 +59,9 @@ export const LoginPage = () => {
                                         Forgot your password?
                                     </a>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <Input id="password" name="password" type="password" required />
                             </div>
-                            <Button type="submit" className="w-full">
+                            <Button type="submit" className="w-full" disabled={isPosting}>
                                 Login
                             </Button>
                             <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
